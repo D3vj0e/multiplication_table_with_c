de88ed947be5c4045549833474c529b7507c2da1
Y2FsbCBtZSBQcm9mZXNzb1Ig <47165982+D3vj0e@users.noreply.github.com>
2021-07-31 15:03:30 +0700

    add_project

---
+++ multiplication_table_with_c/multiplication_table_with_c.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+//สูตรคูณ
+int main(){
+	int num_input;
+	while(true){
+		printf("\nยินดีต้อนรับสู่โปรแกรมสูตนคูณ by D3vj0e\n");
+		printf("ถ้าต้องการออกให้กด Ctrl + c\n");
+		printf("ใส่ตัวเลขที่ต้องการ : ");
+		scanf("%d",&num_input);
+		for(int i = 1; num_input != 0;i++){
+			printf("\n%d x %d = %d",num_input,i,num_input*i);
+			if(i == 12){
+				break;
+			}
+		} 
+	}
+	return(0);
+}
